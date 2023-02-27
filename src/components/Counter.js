825c98ba4284eaae9cc58e8192c48e86893e40dc
Durgesh Mehar <durgeshmehar1380@gmail.com>
2023-02-27 12:55:58 +0530

    implemented the authentication state and managed components by login …

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,34 @@
 import classes from './Counter.module.css';
 import {useSelector, useDispatch} from 'react-redux'
+import {counterAction} from '../Store/index'
 
 const Counter = () => {
-  const counter = useSelector(state=>state.counter);
+  const counter = useSelector(state=>state.Counter.counter);
+  const show = useSelector(state=>state.Counter.showCounter)
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterAction.toggle());
+  };
 
   const increment = () => {
-    dispatch({type:'increment'});
+    dispatch(counterAction.increment());
   }
 
   const decrement = () => {
-    dispatch({type:'decrement'});
+    dispatch(counterAction.decrement());
+  }
+
+  const increase = () => {
+    dispatch(counterAction.increase(3));
   }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+    {show && <div className={classes.value}>{counter}</div>}
       <button onClick={increment}>Increment</button>
+      <button onClick={increase}>increaseBy3</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
